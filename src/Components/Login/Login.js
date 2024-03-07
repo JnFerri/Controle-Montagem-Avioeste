0d236510b784d8306b44712be94dab6fce30e879
Jean Ferri <ferri@avioeste.local>
2024-03-07 14:42:26 -0300

    alterado estilo login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,6 +12,7 @@ align-items:center;
 background-color:white;
 margin:20vh 0px;
 width:50%;
+border-radius:20px;
 `
 
 
@@ -56,7 +57,7 @@ function Login({setLoginConferencia}){
             <Input type="text" value={Usuario} onChange={handleUsuario} placeholder="Coloque o nome de usuario..." padding='15px 0px' margin='10px 0px' border='0.5px solid black' font_size='20px' border_radius='20px' width='80%'></Input>
             <Titulo2>Senha:</Titulo2>
             <Input type="password" value={Senha} onChange={handleSenha} placeholder="Coloque a senha de usuario..." padding='15px 0px' margin='10px 0px' border='0.5px solid black' font_size='20px' border_radius='20px' width='80%'></Input>
-            <Botao onClick={confereLogin} width = '50%' padding='15px 0px' margin='20px 0px' border='0.5px solid black' border_radius='10px' boxshadow='2px 2px 5px black'>Entrar</Botao>
+            <Botao onClick={confereLogin} width = '50%' padding='15px 0px' margin='20px 0px' backgroundcolor='#79b3e0' font_size='26px' border='0.5px solid black' border_radius='10px' boxshadow='2px 2px 5px black'>ENTRAR</Botao>
         </LoginContainer>
     )
 }
