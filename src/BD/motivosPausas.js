cf0cae5e5a790692fa5044fe7188661ac3a6645e
Jean Ferri <ferri@avioeste.local>
2024-04-04 09:52:59 -0300

    adicionado troca de turno aos motivos pausas

--- src/BD/motivosPausas.js
+++ src/BD/motivosPausas.js
@@ -1,4 +1,4 @@
 
-const motivosPausas = ['Falta Material','Falta OP','Retrabalho','Aguardando Qualidade','Falta Feramenta','Limpeza','Aguardando Engenharia', 'Aguardando Manual', 'Manutenção', 'Intervalo']
+const motivosPausas = ['Falta Material','Falta OP','Retrabalho','Aguardando Qualidade','Falta Feramenta','Limpeza','Aguardando Engenharia', 'Aguardando Manual', 'Manutenção', 'Intervalo', 'Troca de Turno']
 
 export default motivosPausas
